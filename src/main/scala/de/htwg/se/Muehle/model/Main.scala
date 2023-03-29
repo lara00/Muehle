16d3d101294f9c12843fb70d5550cc630d10cd9e
Fibe87 <beanoe@gmx.de>
2023-03-29 17:34:47 +0200

    Merge-konflict

--- src/main/scala/de/htwg/se/Muehle/model/Main.scala
+++ src/main/scala/de/htwg/se/Muehle/model/Main.scala
@@ -16,7 +16,7 @@ import de.htwg.se.Muehle.model.{Matrix, Stone, Field}
   println()
   println()
   println()
-  println("Chance a value of a field")
+  println("change a value of a field")
   val s = d.put(Stone.Black, 1, 1)
   println("game board output")
   println(s.mesh())
