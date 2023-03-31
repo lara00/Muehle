15fcaa6f99e003170d2c79754e0bba146532516f
lara00 <93842402+lara00@users.noreply.github.com>
2023-03-31 10:45:58 +0200

    Delete Stone.scala

--- src/main/scala/de/htwg/se/Muehle/model/Stone.scala
+++
@@ -1,8 +0,0 @@
-package de.htwg.se.Muehle.model
-
-enum Stone(string: String):
-  override def toString = string
-  case White extends Stone("WHITE")
-  case Black extends Stone("BLACK")
-  case Empty extends Stone("EMPTY")
-  case Vorbitten extends Stone("")
