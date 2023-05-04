0ffcfd12257e89f20e67e5c0143444558c3adab5
lara00 <93842402+lara00@users.noreply.github.com>
2023-05-05 01:15:21 +0200

    Update Field.scala

--- src/test/scala/de/htwg/se/Muehle/model/Field.scala
+++ src/test/scala/de/htwg/se/Muehle/model/Field.scala
@@ -5,24 +5,40 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 class FieldSpec extends AnyWordSpec with Matchers {
+  val field = Field()
   "A Field" when {
     "its size is checked" should {
       "return 24" in {
-        val field = Field()
         field.size should be(24)
       }
     }
     "modified using setField" should {
+      val newField = field.setStone(1, Stone.Black)
+      val field2 = newField.setStone(1, Stone.Black)
+      val field3 = newField.setStone(1, Stone.White)
       "return a new Field with the specified value at the specified position" in {
-        val field = Field()
-        val newField = field.setField(1, Stone.Black)
         newField.fields(1) should be(Stone.Black)
+        field2.fields(1) should be(Stone.Black)
+        field3.fields(1) should be(Stone.Black)
+      }
+    }
+    "isFieldValid" should {
+      "return true for a valid integer within range" in {
+        val validInt = "12"
+        field.isFieldValid(validInt) shouldBe true
+      }
+      "return false for an integer above the maximum range" in {
+        val invalidInt = "30"
+        field.isFieldValid(invalidInt) shouldBe false
+      }
+      "return false for a non-integer input" in {
+        val invalidInput = "abc"
+        field.isFieldValid(invalidInput) shouldBe false
       }
     }
     "printed" should {
       "display empty stones in all fields" in {
-        val field = Field()
-        field.printfeld() should be(
+        field.toString() should be(
           "EMPTY |       |       | EMPTY |       |       | EMPTY" + "\n" +
             "      | EMPTY |       | EMPTY |       | EMPTY |      " + "\n" +
             "      |       | EMPTY | EMPTY | EMPTY |       |      " + "\n" +
