23a17ca0fbd924564bd296232640ed6e990fbf83
lara00 <lara@geyer-rv.de>
2023-05-18 02:32:32 +0200

    add pattern

--- src/main/scala/de/htwg/se/Muehle/Main.scala
+++ src/main/scala/de/htwg/se/Muehle/Main.scala
@@ -3,14 +3,20 @@ package de.htwg.se.Muehle
 import de.htwg.se.Muehle.aview.Tui
 import scala.io.StdIn.readLine
 import controller.Controller
-import model.{Field, PlayerList}
+import model.{Field, PlayerList, Stone}
+import model.GameStap
+import model.HumanPlayer
 
 object Main {
   def main(args: Array[String]): Unit = {
     println("Play Muehle")
     val field = Field()
     val players = PlayerList(7)
-    val controller = Controller(field, players.getFirstPlayer, players)
+    val humanPlayer = HumanPlayer()
+    val controller = Controller(
+      GameStap(field, players.getFirstPlayer, players),
+      humanPlayer
+    )
     val tui = Tui(controller)
     tui.run
   }
