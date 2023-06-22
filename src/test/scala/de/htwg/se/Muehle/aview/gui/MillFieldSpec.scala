3212a2bf42d01c0274bcd4959638128e3d959e0a
lara00 <lara@geyer-rv.de>
2023-06-22 17:18:42 +0200

    add test

--- src/test/scala/de/htwg/se/Muehle/aview/gui/MillFieldSpec.scala
+++ src/test/scala/de/htwg/se/Muehle/aview/gui/MillFieldSpec.scala
@@ -17,7 +17,6 @@ import java.awt.Color
 class MillFieldSpec extends AnyFlatSpec with Matchers {
   "MillField" should "update player statistics and game stand label" in {
     val con = Controller()
-    val e = SettingsDialog(con)
     System.setProperty("java.awt.headless", "true")
     SwingUtilities.invokeLater(new Runnable {
       override def run(): Unit = {
