445fd55bbfb280f3f20deaced4234189bf3c82ec
lara00 <lara@geyer-rv.de>
2023-06-23 01:39:18 +0200

    change settingDialog

--- src/main/scala/de/htwg/se/Muehle/aview/gui/SettingsDialog.scala
+++ src/main/scala/de/htwg/se/Muehle/aview/gui/SettingsDialog.scala
@@ -9,14 +9,13 @@ class SettingsDialog(controller: IController) extends Dialog:
   title = "Settings"
   val sizes = Seq(3, 4, 5, 6, 7, 8, 9)
   val singleplayer = Seq(false, true)
-
-  val buttons = sizes.flatMap { size =>
-    singleplayer.map { single =>
-      val radioButton = new RadioButton(s"${if (single) "Singel " else ""} Game ($size x $size)")
-      radioButton.reactions += { case ButtonClicked(_) => controller.bildGameSet(size, single)
-      }
-      radioButton
-    }}
-
+  
   contents = new BoxPanel(Orientation.Vertical):
-    contents ++= buttons
+    contents ++= sizes.flatMap { size =>
+      singleplayer.map { single =>
+        val radioButton = new RadioButton(s"${if (single) "Single " else ""}Game ($size x $size)")
+        radioButton.reactions += { case ButtonClicked(_) => controller.bildGameSet(size, single) }
+        radioButton
+      }
+    }
+
