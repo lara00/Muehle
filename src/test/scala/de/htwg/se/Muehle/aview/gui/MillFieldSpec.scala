7fd5e53c1ebc94373d4c8b9dabd24be650be1742
lara00 <lara@geyer-rv.de>
2023-06-22 15:25:14 +0200

    change gui test

--- src/test/scala/de/htwg/se/Muehle/aview/gui/MillFieldSpec.scala
+++ src/test/scala/de/htwg/se/Muehle/aview/gui/MillFieldSpec.scala
@@ -16,7 +16,6 @@ import java.awt.Graphics2D
 class MillFieldSpec extends AnyFlatSpec with Matchers {
   "MillField" should "update player statistics and game stand label" in {
     val con = Controller()
-    val t = QuitConfirmationDialog(con)
     System.setProperty("java.awt.headless", "true")
     SwingUtilities.invokeLater(new Runnable {
       override def run(): Unit = {
