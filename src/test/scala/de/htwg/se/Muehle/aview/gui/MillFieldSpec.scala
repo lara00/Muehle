102a2db4c1485ca5e36a7433680d20152e52b639
lara00 <lara@geyer-rv.de>
2023-06-23 00:45:14 +0200

    MillFieldSpec update

--- src/test/scala/de/htwg/se/Muehle/aview/gui/MillFieldSpec.scala
+++ src/test/scala/de/htwg/se/Muehle/aview/gui/MillFieldSpec.scala
@@ -18,6 +18,7 @@ class MillFieldSpec extends AnyFlatSpec with Matchers {
   "MillField" should "update player statistics and game stand label" in {
     val con = Controller()
     val showstones = ShowStones(con)
+    val bis = SettingsDialog(con)
     SwingUtilities.invokeLater(new Runnable {
       override def run(): Unit = {
         val millField = MillField(con)
